dadf5e6e0ac52e4cb2a364dec9c57e8c56cbda20
Mert <mertuygur02@gmail.com>
2021-03-09 11:43:15 +0300

    her

--- client/src/components/Movie.js
+++ client/src/components/Movie.js
@@ -1,20 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Movies from './Movies/Movies.js';
 
 const Movie = () => {
+
+    const [title, setTitle] = useState("");
+    const [message, setMessage] = useState("");
+
+    const MovieTitleUpdate = (e) => {
+        setTitle(e.target.value);
+    }
+    const MovieMessageUpdate = (e) => {
+        setMessage(e.target.value);
+    }
+
+    const handleSubmit = () => {
+
+        const postURL = "https://from-the-heap.herokuapp.com/movies";
+
+        fetch(postURL, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                title: title,
+                message: message,
+            })
+        })
+            .then(() => {
+                alert('Successfully added.')
+            })
+
+    }
     return (
         <div className="movie">
 
             <div className="movie__left">
                 <div className="movie__left__throw">
-                    <form autoComplete="off">
+                    <form autoComplete="off" onSubmit={handleSubmit}>
                         <label htmlFor="movie__title">Title</label>
-                        <input type="text" id="movie__title" />
+                        <input type="text" id="movie__title" required onChange={MovieTitleUpdate} />
 
                         <label htmlFor="movie__message">Your Message</label>
-                        <input type="text" id="movie__message" />
-                        <button></button>
+                        <input type="text" id="movie__message" onChange={MovieMessageUpdate} />
+                        <button type="submit"></button>
                     </form>
                 </div>
 
