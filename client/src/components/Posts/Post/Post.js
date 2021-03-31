d212ba8ec409e122d9f0c8cc6b509ad8e79cdbe8
Mert <mertuygur02@gmail.com>
2021-03-31 13:25:42 +0300

    some scss things

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import spotify from '../../../assets/spotify.svg'
+import youtube from '../../../assets/youtube.svg'
 
 const Post = ({ post }) => {
 
@@ -13,8 +15,8 @@ const Post = ({ post }) => {
             </div>
 
             <div className="post__links">
-                <a href={post.spotify} target="_blank" rel="noreferrer">Spotify</a>
-                <a href={post.youtube} target="_blank" rel="noreferrer">Youtube</a>
+                <a href={post.spotify} target="_blank" rel="noreferrer"><img src={spotify} alt="spotify icon" width="100px" /></a>
+                <a href={post.youtube} target="_blank" rel="noreferrer"><img src={youtube} alt="youtube icon" width="100px" /></a>
             </div>
         </div>
     )
