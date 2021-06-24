f392306d4bcd05a174d8a0312ce9db26482914ba
hachiko <mertuygur02@gmail.com>
2021-06-24 20:28:48 +0300

    help me get started

--- server/controllers/posts.js
+++
@@ -1,9 +0,0 @@
-
-const Song = require('../models/song.js');
-
-
-exports.createsong = (req, res) => {
-    const {artist, title, thoughts} = req.body;
-
-    const song = new Song({artist, title, thoughts})
-}
